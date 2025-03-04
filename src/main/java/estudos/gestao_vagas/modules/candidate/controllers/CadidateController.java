48e0adc7c2c26e9d377dc869807a4cf988a03556
tnegro <tiago.negro1@hotmail.com>
2025-03-04 20:35:13 -0300

    inserindo validações

--- src/main/java/estudos/gestao_vagas/modules/candidate/controllers/CadidateController.java
+++ src/main/java/estudos/gestao_vagas/modules/candidate/controllers/CadidateController.java
@@ -1,18 +1,19 @@
 package estudos.gestao_vagas.modules.candidate.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import estudos.gestao_vagas.modules.candidate.CandidateEntity;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidate")
 public class CadidateController {
 
-    @GetMapping("/")
-    public void create(@RequestBody CandidateEntity candidateEntity) {
+    @PostMapping("/")
+    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
         System.out.println("Candidato");
         System.out.println(candidateEntity.getEmail());
     }
