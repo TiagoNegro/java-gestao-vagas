507a43d0a5387687c466027a8ab686c5d5ef473b
tnegro <tiago.negro1@hotmail.com>
2025-03-04 20:57:19 -0300

    formatando mensagem de erro

---
+++ src/main/java/estudos/gestao_vagas/expections/ErrorMessageDTO.java
@@ -0,0 +1,11 @@
+package estudos.gestao_vagas.expections;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorMessageDTO {
+    private String message;
+    private String field;
+}
--- src/main/java/estudos/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/estudos/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.hibernate.validator.constraints.Length;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
@@ -13,13 +14,14 @@ public class CandidateEntity {
     private UUID id;
     private String name;
 
+    @NotBlank
     @Pattern(regexp = "\\S+", message = "O campo[username] não deve ter espaço")
     private String username;
 
     @Email(message = "O campo [e-mail] deve conter um e-mail válido")
     private String email;
 
-    @Length(min=10, max=100)
+    @Length(min=10, max=100, message = "O campo [password] deve conter no mínimo 10 caracteres e no máximo 100")
     private String password;
     private String description;
     private String curriculum;
